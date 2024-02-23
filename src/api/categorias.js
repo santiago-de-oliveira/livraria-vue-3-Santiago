935b599acb64231a8f5a34049d87c8b3ff1b56f3
Marco André Mendes <marcoandre@gmail.com>
2024-02-23 11:16:36 -0300

    Adiciona dependência dotenv e configuração prévia para desenvolvimento

--- src/api/categorias.js
+++ src/api/categorias.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 export default class CategoriasApi {
   async buscarTodasAsCategorias() {
-    const { data } = await axios.get('/categorias/');
-    return data;
+    const { data } = await axios.get("/categorias/");
+    return data.results;
   }
   async adicionarCategoria(categoria) {
-    const { data } = await axios.post('/categorias/', categoria);
-    return data;
+    const { data } = await axios.post("/categorias/", categoria);
+    return data.results;
   }
   async atualizarCategoria(categoria) {
     const { data } = await axios.put(`/categorias/${categoria.id}/`, categoria);
-    return data;
+    return data.results;
   }
   async excluirCategoria(id) {
     const { data } = await axios.delete(`/categorias/${id}/`);
-    return data;
+    return data.results;
   }
 }
