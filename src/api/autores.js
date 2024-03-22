2690e487664de81befdbe15f93d8405b19406430
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-03-22 16:05:07 -0300

    10

--- src/api/autores.js
+++ src/api/autores.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default class AutoresApi {
   async buscarTodosOsAutores() {
     const { data } = await axios.get("/autores/");
-    return data.results;
+    return data;
   }
   async adicionarAutor(autor) {
     const { data } = await axios.post("/autores/", autor);
-    return data.results;
+    return data;
   }
   async atualizarAutor(autor) {
     const { data } = await axios.put(`/autores/${autor.id}/`, autor);
-    return data.results;
+    return data;
   }
   async excluirAutor(id) {
     const { data } = await axios.delete(`/autores/${id}/`);
-    return data.results;
+    return data;
   }
 }
--- src/api/editoras.js
+++ src/api/editoras.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default class EditorasApi {
   async buscarTodasAsEditoras() {
     const { data } = await axios.get("/editoras/");
-    return data.results;
+    return data;
   }
   async adicionarEditora(editora) {
     const { data } = await axios.post("/editoras/", editora);
-    return data.results;
+    return data;
   }
   async atualizarEditora(editora) {
     const { data } = await axios.put(`/editoras/${editora.id}/`, editora);
-    return data.results;
+    return data;
   }
   async excluirEditora(id) {
     const { data } = await axios.delete(`/editoras/${id}/`);
-    return data.results;
+    return data;
   }
 }
